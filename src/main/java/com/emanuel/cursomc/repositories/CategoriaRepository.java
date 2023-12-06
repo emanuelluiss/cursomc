03f77d1f9d96fa38f777c64ea27ee6bbb74da9ad
Emanuel Luiz <psyrotechnician@gmail.com>
2023-12-06 14:28:06 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/emanuel/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.emanuel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.emanuel.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+    
+}
